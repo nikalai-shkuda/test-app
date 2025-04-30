21f1be72819f0364f9cac89fce88fe33b45622b4
Nikalai Shkuda <nikolayshkuda@mail.ru>
2025-04-30 14:33:30 +0300

    feat(albums) add styles for photo gallery

--- src/entities/albums/AlbumCart.tsx
+++ src/entities/albums/AlbumCart.tsx
@@ -2,9 +2,20 @@ import { BASE_URL } from "../../shared/constants";
 import { useFetch } from "../../shared/hooks/useFetch";
 import { AlbumThumbnail } from "./AlbumThumbnail";
 import { AlbumCartProps, AlbumPhoto } from "./types";
+import styles from "./AlbumCart.module.css";
 
 const PHOTOS_LIMIT = 3;
 const IMAGE_URL = "https://placehold.co/150x150/92c952/fff";
+
+// const styles = {
+//   list: {
+//     display: "flex",
+//     gap: "0.5rem",
+//     listStyle: "none",
+//     padding: "0",
+//     margin: "0",
+//   },
+// };
 
 export function AlbumCart({ album }: AlbumCartProps) {
   const PHOTOS_URL = `${BASE_URL}/photos?albumId=${album.id}&_limit=${PHOTOS_LIMIT}`;
@@ -17,7 +28,7 @@ export function AlbumCart({ album }: AlbumCartProps) {
       {isLoading && !isError && <div>Loading...</div>}
       {!isLoading && isError && <div>Error, try again</div>}
       {!isLoading && !isError && (
-        <ul>
+        <ul className={styles.list}>
           {photos.map((photo) => (
             <li key={photo.id}>
               <AlbumThumbnail
